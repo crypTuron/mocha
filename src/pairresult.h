ab084fb4ce92dd272f41a9a9df9f6b15149e78bd
root <root@0912356.localdomain>
2020-05-18 17:47:06 -0500

    Initial commit

---
+++ src/pairresult.h
@@ -0,0 +1,21 @@
+// Copyright (c) 2019-2023 The MOCHA developers
+// Copyright (c) 2019 The PIVX developers
+// Distributed under the MIT software license, see the accompanying
+// file COPYING or http://www.opensource.org/licenses/mit-license.php.
+
+#ifndef MOCHA_PAIRRESULT_H
+#define MOCHA_PAIRRESULT_H
+
+
+class PairResult {
+
+public:
+    PairResult(bool res):result(res){}
+    PairResult(bool res, std::string* statusStr):result(res), status(statusStr){}
+
+    bool result;
+    std::string* status = nullptr;
+};
+
+
+#endif //MOCHA_PAIRRESULT_H
