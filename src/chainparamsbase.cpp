1851d9a6dcbadb0a52c804ba1c6355b5838348e2
WillWammer <60303987+WillWammer@users.noreply.github.com>
2020-05-20 12:23:05 -0600

    Update chain RPC ports

--- src/chainparamsbase.cpp
+++ src/chainparamsbase.cpp
@@ -23,7 +23,7 @@ public:
     CBaseMainParams()
     {
         networkID = CBaseChainParams::MAIN;
-        nRPCPort = 8232;
+        nRPCPort = 21104;
     }
 };
 static CBaseMainParams mainParams;
@@ -37,7 +37,7 @@ public:
     CBaseTestNetParams()
     {
         networkID = CBaseChainParams::TESTNET;
-        nRPCPort = 18232;
+        nRPCPort = 29813;
         strDataDir = "testnet4";
     }
 };
